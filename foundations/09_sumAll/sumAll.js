0cdd5aa0b29de0969d121ec05465b744bd6f41e6
Thanningham <aethaniel.nazareno.22@gmail.com>
2026-06-24 23:58:25 +0800

    Finish sumAll exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a , b) {
+    let startingNum;
+    let endingNum;
+    if (a < b) {
+        startingNum = a;
+        endingNum = b;
+    } else {
+        startingNum = b;
+        endingNum = a;
+    }
+    if (!Number.isInteger(startingNum) || 
+        !Number.isInteger(endingNum) ||
+        startingNum < 0 ||
+        endingNum < 0) {
+        return "ERROR";
+    }
 
+    let total = 0;
+    for (let i = startingNum; i <= endingNum; i++) {
+        total += i
+    }
+    return total;
 };
 
 // Do not edit below this line
