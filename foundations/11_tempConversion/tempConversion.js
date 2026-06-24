351b96ce3955e42de3280d490d251297b65ef572
Thanningham <aethaniel.nazareno.22@gmail.com>
2026-06-25 07:29:06 +0800

    Finish tempConversion exercise

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let convertedNum = (fahrenheit - 32) * (5 / 9);
+  return +convertedNum.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let convertedNum = (celsius * 1.8) + 32;
+  return +convertedNum.toFixed(1);
 };
 
 // Do not edit below this line
