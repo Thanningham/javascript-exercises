f9894ae4c7a7a998384972af745816d8111d859b
Thanningham <aethaniel.nazareno.22@gmail.com>
2026-06-24 19:02:12 +0800

    Finish removeFromArray exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elemToRemove) {
+    let finalArr = arr.slice();
+    for (const elem of elemToRemove) {
+        finalArr = finalArr.filter((item) => item !== elem)
+    }
+    return finalArr;
 };
 
 // Do not edit below this line
