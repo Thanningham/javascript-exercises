ad32b8e393af00692fdea551426a211cbf649615
Thanningham <aethaniel.nazareno.22@gmail.com>
2026-07-07 01:26:50 +0800

    Solve palindromes exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    let formattedString = ""
+    let reverseString = "";
 
+    for (let i = 0; i < string.length; i++) {
+        if (/^[a-z0-9]$/i.test(string[i])) {
+            formattedString += string[i];
+        }
+    }
+
+    for (let i = formattedString.length - 1; i >= 0; i--) {
+        reverseString += formattedString[i];
+    }
+
+    return reverseString === formattedString;
 };
 
 // Do not edit below this line
