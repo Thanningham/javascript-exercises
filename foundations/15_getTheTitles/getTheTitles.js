81f997b2eae09fcffa5f40aa8dc12b8476570cd4
Thanningham <aethaniel.nazareno.22@gmail.com>
2026-07-08 09:52:10 +0800

    Solve getTheTitles exercise

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titleArr = books.map(book => book.title);
+    return titleArr;
 };
 
 // Do not edit below this line
