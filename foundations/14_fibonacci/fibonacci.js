bb3a1680a9f2e720db7db8f00d40fbd0bfd004fe
Thanningham <aethaniel.nazareno.22@gmail.com>
2026-07-08 09:34:35 +0800

    Solve fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(currentTerm) {
+    let fibbonaciArr = [0,1];
+    
+    if (currentTerm < 0) {
+        return `OOPS`;
+    }
 
+    for(let i = 2; i <= currentTerm; i++) {
+        fibbonaciArr.push(fibbonaciArr[i - 1] + fibbonaciArr[i - 2]);
+    }
+
+    return fibbonaciArr[currentTerm]
 };
 
 // Do not edit below this line
