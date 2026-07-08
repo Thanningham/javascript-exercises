c2833619b9625e45c87728a6cfd94786728ebe38
Thanningham <aethaniel.nazareno.22@gmail.com>
2026-07-08 10:43:11 +0800

    Solve findTheOldest exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArr) {
+    const currentYear = new Date().getFullYear();
+    
+    const age = peopleObj => (peopleObj.yearOfDeath ?? currentYear) - peopleObj.yearOfBirth;
 
+    let oldest = peopleArr[0];
+
+    for (const person of peopleArr) {
+        if (age(person) > age(oldest)) {
+            oldest = person;
+        }
+    }
+
+    return oldest;
 };
 
 // Do not edit below this line
