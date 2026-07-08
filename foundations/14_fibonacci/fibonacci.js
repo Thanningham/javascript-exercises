03c8c96211d179e1062e6e0cadb63013a497b614
Thanningham <aethaniel.nazareno.22@gmail.com>
2026-07-08 09:43:15 +0800

    Improve fibbonaci exercise solution

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,15 +1,18 @@
-const fibonacci = function(currentTerm) {
-    let fibbonaciArr = [0,1];
+const fibonacci = function(nthFibb) {
     
-    if (currentTerm < 0) {
-        return `OOPS`;
+    if (nthFibb < 0) return `OOPS`;
+    if (nthFibb == 0) return 0;
+
+    let previous = 0;
+    let current = 1;
+    
+    for(let i = 2; i <= nthFibb; i++) {
+        let next = previous + current;
+        previous = current;
+        current = next;
     }
 
-    for(let i = 2; i <= currentTerm; i++) {
-        fibbonaciArr.push(fibbonaciArr[i - 1] + fibbonaciArr[i - 2]);
-    }
-
-    return fibbonaciArr[currentTerm]
+    return current;
 };
 
 // Do not edit below this line
